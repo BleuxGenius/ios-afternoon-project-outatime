6eff0e9f8981cdadcf5d18069ef79fe0d0d75e05
Dani <bleuxgenius@gmail.com>
2019-08-21 15:58:19 -0500

    UI Completed

---
+++ Outatime/Outatime/TimeCircuitViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TimeCircuitViewController.swift
+//  Outatime
+//
+//  Created by Lambda_School_Loaner_167 on 8/21/19.
+//  Copyright © 2019 Lambda_School_Loaner_167. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitViewController: UIViewController {
+
+    @IBOutlet weak var timeCircuitLabel: UILabel!
+    @IBOutlet weak var datePicker: UIPickerView!
+    
+    @IBAction func travelBackButtonTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func datePickerTapped(_ sender: UIPickerView) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
